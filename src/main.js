5960de3484fb53e809191fb6f8bb64bcc2aa89ce
jensuki <jeni.suk@proton.me>
2025-03-15 20:12:46 -0700

    complete movie detail template with stats and poster

--- src/main.js
+++ src/main.js
@@ -34,7 +34,7 @@ const autoCompleteConfig = {
         s: searchTerm
       }
     });
-    console.log(response.data);
+
     return response.data.Error ? [] : response.data.Search;
   }
 };
@@ -78,9 +78,47 @@ const movieTemplate = (movieDetail) => {
       return isNaN(value) ? total : total + value;
     }, 0)
     : 0;
-}
 
-return `
+  return `
+    <article class="media">
+      <figure class="media-left">
+        <p class="image">
+          <img src="${movieDetail.Poster}" />
+        </p>
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <h1>${movieDetail.Title}</h1>
+          <h4>${movieDetail.Genre}</h4>
+          <p>${movieDetail.Plot}</p>
+        </div>
+      </div>
+    </article>
 
+    <article class="notification is-primary is-rounded">
+      <h4 class="title is-4">${movieDetail.Awards}</h4>
+      <p class="subtitle">Awards</p>
+    </article>
+
+    <article class="notification is-primary is-rounded">
+      <h4 class="title is-4">${movieDetail.BoxOffice}</h4>
+      <p class="subtitle">Box Office</p>
+    </article>
+
+    <article class="notification is-primary is-rounded">
+      <h4 class="title is-4">${movieDetail.Metascore}</h4>
+      <p class="subtitle">Metascore</p>
+    </article>
+
+    <article class="notification is-primary is-rounded">
+      <h4 class="title is-4">${movieDetail.imdbRating}</h4>
+      <p class="subtitle">IMDB Rating</p>
+    </article>
+
+    <article class="notification is-primary is-rounded">
+      <h4 class="title is-4">${movieDetail.imdbVotes}</h4>
+      <p class="subtitle">IMDB Votes</p>
+    </article>
 `;
 
+};
